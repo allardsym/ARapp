f315884cf0024ad530c34418cd7c0626999c9a5e
Allard Sym Sprenger <allardsymsprenger@gmail.com>
2022-11-26 21:03:13 +0100

    No real changes

---
+++ ARapp/ARapp_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void main::Awake()
+extern void main_Awake_m151162562BC861BE7A3B3254BAB8E83C5F50C14C (void);
+// 0x00000002 System.Void main::Update()
+extern void main_Update_m8A9D6818CB69459A554B68231329A0100C1D2830 (void);
+// 0x00000003 System.Void main::OnEnable()
+extern void main_OnEnable_m15A0F13ABE938A6AD296AE8DFEF935707D117D1F (void);
+// 0x00000004 System.Void main::OnDisable()
+extern void main_OnDisable_m021DB4D8881AC63AFA98177BCE6FB7E048DEA39D (void);
+// 0x00000005 System.Void main::OnTrackedImagesChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void main_OnTrackedImagesChanged_m8CEF60540D6A5CD1E43403C6B6BCB1D9E8310C80 (void);
+// 0x00000006 System.Void main::UpdateARImage(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void main_UpdateARImage_m4BE3F5BED26ED2FF7719964FDA58C1E04B864C4C (void);
+// 0x00000007 System.Void main::.ctor()
+extern void main__ctor_m130E0270AE02CDA3754528B71F901B97C4AE91BA (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	main_Awake_m151162562BC861BE7A3B3254BAB8E83C5F50C14C,
+	main_Update_m8A9D6818CB69459A554B68231329A0100C1D2830,
+	main_OnEnable_m15A0F13ABE938A6AD296AE8DFEF935707D117D1F,
+	main_OnDisable_m021DB4D8881AC63AFA98177BCE6FB7E048DEA39D,
+	main_OnTrackedImagesChanged_m8CEF60540D6A5CD1E43403C6B6BCB1D9E8310C80,
+	main_UpdateARImage_m4BE3F5BED26ED2FF7719964FDA58C1E04B864C4C,
+	main__ctor_m130E0270AE02CDA3754528B71F901B97C4AE91BA,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	5205,
+	5205,
+	5205,
+	5205,
+	4051,
+	4146,
+	5205,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
